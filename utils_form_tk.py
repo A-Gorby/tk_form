2614f9ff499da5fd154f950516ad3f797c80e67b
A-Gorby <123462010+A-Gorby@users.noreply.github.com>
2023-06-23 15:05:55 +0300

    Add files via upload

--- utils_form_tk.py
+++ utils_form_tk.py
@@ -130,6 +130,6 @@ def form_param_esklp_exist_dicts(esklp_dates):
     check_box = Box([Label(value="Выберите дату сохраненного справочника ЕСКЛП:"), esklp_dates_dropdown], layout=form_item_layout)
     form_items = [check_box]
 
-    form_esklp_exist_dicts = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='auto')) #width='90%')) #
+    form_esklp_exist_dicts = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='50%')) #width='50%')) #
     # return form, fn_check_file_drop_douwn, fn_dict_file_drop_douwn, radio_btn_big_dict, radio_btn_prod_options, similarity_threshold_slider, max_entries_slider
     return form_esklp_exist_dicts, esklp_dates_dropdown
